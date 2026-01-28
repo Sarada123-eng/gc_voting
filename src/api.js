c3f8a410bc635c93f61f855655d2819756f2c59e
Sarada Prasanna Das <jkdas73@gmail.com>
2026-01-29 02:39:17 +0530

    updated backend url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-const API_BASE = import.meta.env.VITE_API_BASE_URL;
+const API_BASE = "https://gc-voting-system-backend.onrender.com/api";
 
 export async function apiRequest(endpoint, method = "GET", body) {
   const token = localStorage.getItem("token");
