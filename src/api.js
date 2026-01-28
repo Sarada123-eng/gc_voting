ff35ba6de2e3af724aceedb51543302d4d9a9da2
Sarada Prasanna Das <jkdas73@gmail.com>
2026-01-29 02:21:40 +0530

    added vercel.json and updated backend url

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 
-const API_BASE = "http://localhost:3000/api";
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 
 export async function apiRequest(endpoint, method = "GET", body) {
   const token = localStorage.getItem("token");
